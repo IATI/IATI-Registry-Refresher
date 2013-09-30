62257e66ab1d92f108d0fdfb627c33cdf3a121da
Ben Webb <git@bjwebb.co.uk>
2013-09-30 12:26:17 +0100

    Upload logs to github gist

--- git.sh
+++ git.sh
@@ -10,9 +10,15 @@ cd ..
 rm urls/*
 php grab_urls.php
 ./fetch_data.sh 2> debug > errors
+gist errors -u 6726204
+gist debug -u 6726200
 cd data/
 git checkout master -- README.md
 git add .
 git commit -a -m "Automatic data refresh"
 git push
+
+cd ..
+xmllint data/*/* > /dev/null 2> xmlerrors
+gist xmlerrors -u 6726333
 
