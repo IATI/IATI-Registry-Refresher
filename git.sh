c2a4f0f858d43e2b9c468c54096ffe56ba1bfcac
Ben Webb <bjwebb67@googlemail.com>
2013-10-15 13:17:36 +0100

    Split out validation routines for easier testing

--- git.sh
+++ git.sh
@@ -48,46 +48,5 @@ git push
 #### Post processing on our new data snapshot.
 # Removing any lines below will not affect snapshot creation
 
-# Check for xml well formedness errors
 cd ..
-xmllint data/*/* > /dev/null 2> xmlerrors
-# Prevent empty gist that won't be uploaded
-echo "." >> xmlerrors
-gist xmlerrors -u 6726333
-
-# List all root elements in our xml files
-for f in data/*/*; do echo "`xmllint --xpath "name(/*)" "$f"` $f"; done > topelements
-# Upload to github gist all root elements that have names we don't expect
-cat topelements | grep -v iati-activities | grep -v iati-organisations > nonstandardroots
-# Prevent empty gist that won't be uploaded
-echo "." >> nonstandardroots
-gist -u 6728773 nonstandardroots 
-
-# List the version attribute of the root element of all xml files
-for f in data/*/*; do echo "`xmllint --xpath "string(/*/@version)" "$f"` $f"; done > versions
-# Summarise these
-awk -F '[ ]' '{print $1}' versions | sort | uniq -c > 0_versions-summary
-gist -u 6729360 0_versions-summary versions
-
-
-# Validate all the files
-for f in data/*/*; do echo $f; done > list   
-for f in data/*/*; do 
-    topel="`xmllint --xpath "name(/*)" "$f"`"
-    version="`xmllint --xpath "string(/*/@version)" "$f"`"
-    if [ "$version" == "1.01" ] || [ "$version" == "1" ] || [ "$version" == "1.0" ] || [ "$version" == "1.00" ]; then version="1.01";
-    elif [ "$version" == "1.02" ]; then version="1.02";
-    else version="1.03"; fi
-    if [ "$topel" == "iati-activities" ]; then
-        xmllint --schema ~/iati/downloads/$version/iati-activities-schema.xsd --noout "$f" 2> /dev/null
-        if [ $? -eq 0 ]; then echo $f; fi
-    elif [ "$topel" == "iati-organisations" ]; then
-        xmllint --schema ~/iati/downloads/$version/iati-organisations-schema.xsd --noout "$f" 2> /dev/null
-        if [ $? -eq 0 ]; then echo $f; fi
-    fi
-done > list-validate
-comm -23 list list-validate > list-fail-validate
-
-wc -l list* | sed '/total/ d' > 0_list-summary
-gist -u 6931534 0_list-summary list-fail-validate
-
+GIST=true ./validate.sh
